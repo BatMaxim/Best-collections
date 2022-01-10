ee59da37854110f3261a900e3974a5eb7b6097a4
Maksim <gato-v@yandex.ru>
2022-01-10 21:47:24 +0300

    Added group of checkboxes

--- front/src/components/UserItem/UserItem.js
+++ front/src/components/UserItem/UserItem.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTools } from '@fortawesome/free-solid-svg-icons'
+import {Checkbox} from "@createnl/grouped-checkboxes";
 const UserItem = ({user}) => {
     return(
         <tr>
-            <td><input type="checkbox"/></td>
+            <td><Checkbox value={user.uid}/></td>
             <td> {user?.customClaims?.admin && <FontAwesomeIcon icon={faTools} /> } {user.email}</td>
             <td>{user.uid}</td>
             <td></td>
--- front/src/components/UsersTable/UsersTable.js
+++ front/src/components/UsersTable/UsersTable.js
@@ -1,24 +1,31 @@
 import React from "react";
 import {Table} from "react-bootstrap";
 import UserItem from "../UserItem/UserItem";
+import {AllCheckerCheckbox, CheckboxGroup} from "@createnl/grouped-checkboxes";
 
 const UsersTable = ({users}) => {
+    const onCheckboxChange = (checkboxes) => {
+        console.log(checkboxes);
+    }
+
     return(
-        <Table responsive="sm">
-            <thead>
-            <tr>
-                <th>#</th>
-                <th>Email</th>
-                <th>ID</th>
-                <th>Ban status</th>
-            </tr>
-            </thead>
-            <tbody>
-            {
-                users.map(user=><UserItem key={user.uid} user={user}/>)
-            }
-            </tbody>
-        </Table>
+        <CheckboxGroup onChange={onCheckboxChange}>
+            <Table responsive="sm">
+                <thead>
+                <tr>
+                    <th><AllCheckerCheckbox /></th>
+                    <th>Email</th>
+                    <th>ID</th>
+                    <th>Ban status</th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                    users.map(user=><UserItem key={user.uid} user={user}/>)
+                }
+                </tbody>
+            </Table>
+        </CheckboxGroup>
     )
 }
 
