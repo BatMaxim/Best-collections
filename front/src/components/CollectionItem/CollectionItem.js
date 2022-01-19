411d4f3bf61f278387afef8dc8544b6bdb5b8a90
Maksim <gato-v@yandex.ru>
2022-01-19 12:31:45 +0300

    Added a link to the collection page

--- front/src/components/CollectionItem/CollectionItem.js
+++ front/src/components/CollectionItem/CollectionItem.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const CollectionItem = ({collection}) => {
+    const navigate = useNavigate();
     return(
-            <tr>
+            <tr onClick={()=>{navigate(`/collection/${collection.id}`)}}>
                 <td><img src={collection.picture} alt={collection.name}/></td>
                 <td>{collection.name}</td>
                 <td>{collection.topic.name}</td>
