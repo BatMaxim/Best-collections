33d999be97ab0275ada02cfb64c88c183159fb24
Maksim <gato-v@yandex.ru>
2022-01-19 12:40:26 +0300

    Added CollectionDescription component

---
+++ front/src/components/CollectionDescription/CollectionDescription .js
@@ -0,0 +1,27 @@
+import React from "react";
+import {Button, Card} from "react-bootstrap";
+
+const CollectionDescription = ({collection}) => {
+    return(
+        <Card className="collection">
+            <Card.Header className="collection__header">
+                <div className="collection__img-container">
+                    <img className="collection__img" src={collection.picture}/>
+                    <>Collection: {collection.id}</>
+                </div>
+                <Button variant="secondary">Edit</Button>
+            </Card.Header>
+            <Card.Body>
+                <Card.Title>{collection.name}</Card.Title>
+                <Card.Text>
+                    Author: test@test.ru
+                </Card.Text>
+                <Card.Text>
+                    {collection.description}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    )
+}
+
+export default CollectionDescription;
--- front/src/pages/CollectionPage/CollectionPage.js
+++ front/src/pages/CollectionPage/CollectionPage.js
@@ -1,30 +1,13 @@
 import React from "react";
 import {useParams} from "react-router-dom";
-import {Badge, Card, Button} from "react-bootstrap";
 import "./CollectionPage.css";
+import CollectionDescription from "../../components/CollectionDescription/CollectionDescription ";
 
 const CollectionPage = () => {
     let { collectionId } = useParams();
     return(
         <div>
-            <Card className="collection">
-                <Card.Header className="collection__header">
-                    <div className="collection__img-container">
-                        <img className="collection__img" src={"http://via.placeholder.com/100x100"}/>
-                        <>Collection: {collectionId}</>
-                    </div>
-                    <Button variant="secondary">Edit</Button>
-                </Card.Header>
-                <Card.Body>
-                    <Card.Title>Test name</Card.Title>
-                    <Card.Text>
-                       Author: test@test.ru
-                    </Card.Text>
-                    <Card.Text>
-                      Test Description
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+            <CollectionDescription collection={{}}/>
         </div>
     )
 }
