308dce472f23d381926b7e6a4d0ec07d84102ff9
Maksim <gato-v@yandex.ru>
2022-01-20 18:47:53 +0300

    Changed the display of images in the collections table

--- front/src/components/CollectionItem/CollectionItem.js
+++ front/src/components/CollectionItem/CollectionItem.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "./CollectionItem.css"
 
 const CollectionItem = ({collection}) => {
     const navigate = useNavigate();
     return(
             <tr onClick={()=>{navigate(`/collection/${collection.id}`)}}>
-                <td><img src={collection.picture} alt={collection.name}/></td>
+                <td className="collection-item__img-container">
+                    <img src={collection.picture}
+                         alt={collection.name}
+                         className="collection-item__img"/>
+                </td>
                 <td>{collection.name}</td>
                 <td>{collection.topic.name}</td>
                 <td>{collection.description}</td>
