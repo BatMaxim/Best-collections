9bdc2cc5bbfe7c2ae3d0ba393d9aa272886882fa
Maksim <gato-v@yandex.ru>
2022-01-28 15:13:57 +0300

    Changed admin private route

--- front/src/components/AdminPrivatRoute/AdminPrivatRoute.js
+++ front/src/components/AdminPrivatRoute/AdminPrivatRoute.js
@@ -3,7 +3,7 @@ import {Navigate} from "react-router-dom";
 import {useSelector} from "react-redux";
 
 const AdminPrivateRoute = ({ component }) =>{
-    const { admin }= useSelector((state)=>state.user);
-    return admin ? component : <Navigate to="/" />
+    const { admin, uid }= useSelector((state)=>state.user);
+    return (admin && uid) ? component : <Navigate to="/" />
 }
 export default AdminPrivateRoute;
